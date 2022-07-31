f8b0ad5d08c1d25891fefbfc75543b83c5464f36
aryangoyalag <54241797+aryangoyalag@users.noreply.github.com>
2022-07-31 19:35:40 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,6 +4,7 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.preprocessing import StandardScaler
+from fastapi.middleware.cors import CORSMiddleware
 
 class Item(BaseModel):
     LIMIT_BAL: int
@@ -30,6 +31,19 @@ class Item(BaseModel):
 
 
 app = FastAPI()
+origins = [
+    "https://cc-defaulter-checker.vercel.app/",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get('/')
 def index():
     return {'message':'Welcome to Credit Card Default Prediction Model'}
